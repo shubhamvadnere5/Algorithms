47b0c732948d17734e24051080866450b2e1a96c
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-18 21:22:06 +0530

    [ShubhamPV] insertion sorting numbers

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,11 +1,10 @@
-﻿using Algorithms;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AlgorithmProblems
+namespace Algorithms
 {
     class Program
     {
