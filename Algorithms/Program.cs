b158eef6fda0f8bdeb642719a20073e3be3fd6bd
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-18 21:09:51 +0530

    [ShubhamPV] finding number

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +8,41 @@ namespace Algorithms
 {
     class Program
     {
-        static void Main(string[] args)
+        static int searching(int[] array, int first, int last, int value)
         {
-            List<int> list = new List<int>();
-            //Adding Elements in a List
-            list.Add(1);
-            list.Add(5);
-            list.Add(7);
-            list.Add(3);
-            list.Add(17);
-            list.Add(10);
-            list.Add(13);
-            list.Add(9);
-            Console.WriteLine("Unsorted List");
-            list.ForEach(Console.WriteLine);//Displaying UnSorted List
-            Sorting obj = new Sorting();
-            obj.BubbleSort(list);
-            Console.WriteLine("\nSorted List");
-            list.ForEach(Console.WriteLine);//Displaying Sorted List
-            Console.ReadLine();
+            int middle = (first + last) / 2;//finding Middle
+            if (array[middle] == value)
+            {
+                return middle;
+            }
+            if (first >= last)//checking if the array contain more than one element
+            {
+                return -1;
+            }
+            if (value < array[middle])
+            {
+                return searching(array, first, middle, value);
+            }
+            else
+            {
+                return searching(array, middle, last, value);
+            }
+        }
 
+
+
+        // Driver method to test above
+        public static void Main()
+        {
+            var numbers = new[] { 1, 5, 0, 34, 3, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 67, 27, 2, 3, 4, 6, 7, 8, 10 };
+            // Array.Sort(numbers);
+            Console.WriteLine("Think of a number");
+            Console.WriteLine("Enter number you were thinking of ");
+            int num = Convert.ToInt32(Console.ReadLine());//reading a number which you enter
+            var index = searching(numbers, 0, numbers.Length - 1, num);
+            Console.WriteLine("Number {0} is present at index:-", num);
+            Console.WriteLine(index);
+            Console.ReadLine();
         }
     }
 }
